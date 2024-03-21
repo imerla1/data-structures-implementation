4ed72f42d738fa06a3d9e3cae11dafc46bac1bea
imerla1 <giorgi1048@yahoo.com>
2024-03-21 11:53:16 +0400

    add append method, switch __str__ method with __repr__, add __len__ method which returns size of linkedlist.

--- linked_list/ll.py
+++ linked_list/ll.py
@@ -14,14 +14,31 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-    def __str__(self) -> None:
+    def __repr__(self) -> None:
         string_value = ""
         temp = self.head
         while temp is not None:
             string_value += str(temp.value) + "-"
             temp = temp.next
         return string_value
+    
+    def __len__(self) -> int:
+        return self.length
+    
 
-l = LinkedList(5)
-l.head.next = Node(6)
-print(l)
+    def append(self, value: int):
+        node = Node(value)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+        
+
+
+
+l = LinkedList(3)
+l.append(5)
