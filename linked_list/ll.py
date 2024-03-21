55837ec9ab73703c2e417de1be2576278bb59423
imerla1 <giorgi1048@yahoo.com>
2024-03-21 11:59:46 +0400

    update LinkedList class constructor, now it can be empty during initalization

--- linked_list/ll.py
+++ linked_list/ll.py
@@ -8,11 +8,11 @@ class Node:
 
 
 class LinkedList:
-    def __init__(self, value) -> None:
-        new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
+    def __init__(self, value: Union[None, int] = None) -> None:
+        new_node = Node(value) if value else None
+        self.head = new_node if new_node else None
+        self.tail = new_node if new_node else None
+        self.length = 0
 
     def __repr__(self) -> None:
         string_value = ""
@@ -21,10 +21,9 @@ class LinkedList:
             string_value += str(temp.value) + "-"
             temp = temp.next
         return string_value
-    
+
     def __len__(self) -> int:
         return self.length
-    
 
     def append(self, value: int):
         node = Node(value)
@@ -36,9 +35,11 @@ class LinkedList:
             self.tail.next = node
             self.tail = node
         self.length += 1
-        
+
+    def pop(self):
+        if self.head is None:
+            return ...
 
 
-
-l = LinkedList(3)
-l.append(5)
+l = LinkedList()
+# l.append(5)
