808de728c3cc01641d509543f73e4a6f23a549e1
imerla1 <giorgi1048@yahoo.com>
2024-04-08 12:17:01 +0400

    fix bug in constructor and implement insert method in bst.

--- trees/bst.py
+++ trees/bst.py
@@ -1,5 +1,47 @@
 class Node:
-    def __init__(self, value, left = None, right = None) -> None:
+    def __init__(self, value, left=None, right=None) -> None:
         self.value = value
         self.left = left
         self.right = right
+
+
+class BinarySearchTree:
+    # Let's assume in this implementation we don't allow duplication
+    def __init__(self, root_value=None) -> None:
+        new_node = Node(root_value) if root_value is not None else None
+        self.root = new_node if new_node else None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while True:
+            if value == temp.value:
+                return False
+            elif value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                else:
+                    temp = temp.left
+
+            else:
+
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                else:
+                    temp = temp.right
+
+
+my_tree = BinarySearchTree()
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+
+
+print(my_tree.root.value)            
+print(my_tree.root.left.value)        
+print(my_tree.root.right.value)     
