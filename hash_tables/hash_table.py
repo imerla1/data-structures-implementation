95c2041ca3773366439579996be74d7f9e1af3cf
imerla1 <giorgi1048@yahoo.com>
2024-04-09 22:50:19 +0400

    get all keys from hash_table.

--- hash_tables/hash_table.py
+++ hash_tables/hash_table.py
@@ -1,13 +1,18 @@
 class HashTable:
-    def __init__(self, size = 7) -> None:
-        self.data_map = [None] * size 
+    def __init__(self, size=7) -> None:
+        self.data_map = [None] * size
 
     def __hash(self, key):
         my_hash = 0
         for letter in key:
             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
         return my_hash
-    
+
+    def repr_my_hashmap(self):
+        for item in self.data_map:
+            if item is not None:
+                print(item)
+
     def set_item(self, key, value):
         # Let's assume we are using Separate Chaining for collision handling
         index = self.__hash(key)
@@ -21,7 +26,21 @@ class HashTable:
             for i in range(len(self.data_map[index])):
                 if self.data_map[index][i][0] == key:
                     return self.data_map[index][i][1]
-        return None    
+        return None
+
+    def keys(self):
+        all_keys = []
+        for i in self.data_map:
+            if i is not None:
+                for key in i:
+                    get_key = key[0]
+                    all_keys.append(get_key)
+
+        return all_keys
 
 
-
+hash_table = HashTable()
+hash_table.set_item("name", "george")
+hash_table.set_item("name", "lukas")
+hash_table.set_item("age", 25)
+x = hash_table.keys()
