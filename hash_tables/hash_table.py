e4235d9b9288b086bf77145f516774873ffb3fbe
imerla1 <giorgi1048@yahoo.com>
2024-04-09 13:44:58 +0400

    init hashtables

---
+++ hash_tables/hash_table.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+
+class HashTable:
+    def __init__(self, size = 7) -> None:
+        self.data_map = [None] * size 
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+    
