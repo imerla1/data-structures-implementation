08dbf07ffa122345516e400628ecad14eefb8304
imerla1 <giorgi1048@yahoo.com>
2024-04-09 14:00:06 +0400

    implement set_item method in hashTable.

--- hash_tables/hash_table.py
+++ hash_tables/hash_table.py
@@ -1,7 +1,3 @@
-import random
-import string
-
-
 class HashTable:
     def __init__(self, size = 7) -> None:
         self.data_map = [None] * size 
@@ -12,3 +8,9 @@ class HashTable:
             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
         return my_hash
     
+    def set_item(self, key, value):
+        # Let's assume we are using Separate Chaining for collision handling
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
