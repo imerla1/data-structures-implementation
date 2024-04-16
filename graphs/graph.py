0bcf8a942f9bf4dd5feae2db4aa35d8e47f1bad8
imerla1 <giorgi1048@yahoo.com>
2024-04-16 13:35:53 +0400

    fix ValuError if edge alredy delted.

--- graphs/graph.py
+++ graphs/graph.py
@@ -21,9 +21,10 @@ class Graph:
 
     def remove_edge(self, v1, v2):
         if v1 in self.adj_list and v2 in self.adj_list:
-            self.adj_list[v1].remove(v2)
-            self.adj_list[v2].remove(v1)
-            return True
+            if self.adj_list.get(v1) and self.adj_list.get(v2):
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+                return True
         return False
 
     def remove_vertex(self, vertex_name):
