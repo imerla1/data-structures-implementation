ad9c466c0622fb7103d1cca39e52a26634ae31e5
imerla1 <giorgi1048@yahoo.com>
2024-04-16 12:46:27 +0400

    update readme about graphs

---
+++ graphs/graph.py
@@ -0,0 +1,16 @@
+class Graph:
+    def __init__(self) -> None:
+        self.adj_list = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+
+    def add_edges(self, v1, v2):
+        if v1 in self.adj_list and v2 in self.adj_list:
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
